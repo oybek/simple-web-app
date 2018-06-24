7032ac4bde4f7f023121fe44487cad3f1a005bd5
oybek <aybek.hashimov@gmail.com>
2018-06-25 02:59:56 +0500

    prepare to integrate hibernate

--- src/test/java/com/oybek/webapp/WebappApplicationTests.java
+++ src/test/java/com/oybek/webapp/WebappApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringSecurityJdbcAuthenticationPostgreSqlApplicationTests {
+public class WebappApplicationTests {
 
 	@Test
 	public void contextLoads() {
